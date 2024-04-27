b1a59c15d11a2af70babec9d8734b72e99bd0514
18catt <98676305+18catt@users.noreply.github.com>
2024-04-27 23:38:48 +0530

    Update settings.py

--- cv_extractor_project/settings.py
+++ cv_extractor_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@(#m%ok841ii8&j#h-a4hpw6d)ul4n5^119o#dt&fhk=tqeqo#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['placeholder_domain_or_ip']
 
 
 # Application definition
